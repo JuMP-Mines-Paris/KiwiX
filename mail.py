9b4d6df606f5adf6411b6c136c36e973bce7ade2
Antoine Febrier <antoine.febrier@etu.minesparis.psl.eu>
2025-07-23 16:02:19 +0200

    version fonctionnelle

--- mail.py
+++ mail.py
@@ -22,34 +22,22 @@ if len(missions) > 0:
             else:
                 body += f"<b>{key.upper()}</b>: {str(mission[key])}<hr style='border:1px solid #000; margin:10px 0;'>"
         body += f"<hr style='border:5px solid red; margin:60px 0; opacity: 0.7;'>"
-    try:
-        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-            server.starttls()
-            server.login(EMAIL_FROM, EMAIL_PASSWORD)
-
-            for email_to in EMAIL_TO_LIST:
-                msg = MIMEText(body, 'html')
-                msg['Subject'] = subject
-                msg['From'] = EMAIL_FROM
-                msg['To'] = email_to
-
-                server.sendmail(EMAIL_FROM, email_to, msg.as_string())
-    except Exception as e:
-        print("Erreur lors de l'envoi :", e)
 
 
 else:
     body = "Pas de nouvelles missions aujourd'hui"
-    try:
-        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-            server.starttls()
-            server.login(EMAIL_FROM, EMAIL_PASSWORD)
-            email_to = EMAIL_TO_LIST[0]
-            msg = MIMEText(body)
+
+try:
+    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+        server.starttls()
+        server.login(EMAIL_FROM, EMAIL_PASSWORD)
+
+        for email_to in EMAIL_TO_LIST:
+            msg = MIMEText(body, 'html')
             msg['Subject'] = subject
             msg['From'] = EMAIL_FROM
             msg['To'] = email_to
 
             server.sendmail(EMAIL_FROM, email_to, msg.as_string())
-    except Exception as e:
-        print("Erreur lors de l'envoi :", e)
+except Exception as e:
+    print("Erreur lors de l'envoi :", e)
